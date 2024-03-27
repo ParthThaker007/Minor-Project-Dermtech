b45e9b57562e9a66dcfc2c82bf181403d20c24da
ParthThaker007 <101121538+ParthThaker007@users.noreply.github.com>
2024-03-27 13:01:37 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
+// app-routing.module.ts
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LoginPageComponent } from './components/login-page/login-page.component';
-import { SignUpPageComponent } from './components/sign-up-page/sign-up-page.component';
+import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { SignUpPageComponent } from    './components/sign-up-page/sign-up-page.component';
 
-const routes:Routes = [
-  {path:"", redirectTo:"login", pathMatch:"full"},
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirect root path to home
+  { path: 'home', component: HomeComponent},
   {path:"login", component:LoginPageComponent},
   {path:"signUp", component:SignUpPageComponent},
-  {path:"home", component:HomeComponent},
 ];
 
 @NgModule({
@@ -16,3 +17,5 @@ const routes:Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
