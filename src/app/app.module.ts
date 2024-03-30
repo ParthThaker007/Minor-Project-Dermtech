475d1d83e896373b14595598d6fa12ae25133118
ParthThaker007 <101121538+ParthThaker007@users.noreply.github.com>
2024-03-30 21:11:22 +0530

    AuthCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupUpPageComponent } from './components/signup-up-page/signup-up-page.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { HomeComponent } from './components/home/home.component';
 import { SignUpPageComponent } from './components/sign-up-page/sign-up-page.component';
+import {AngularFireModule} from '@angular/fire/compat';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
+import { environment } from '../environment/environment';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +22,12 @@ import { SignUpPageComponent } from './components/sign-up-page/sign-up-page.comp
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+
+
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor(private auth: AngularFireAuth) {}
+
+  login() {
+    console.log('Login button clicked');
+    this.auth.signInWithEmailAndPassword(this.email, this.password)
+      .then((userCredential) => {
+        // Login successful
+        console.log('User logged in:', userCredential.user);
+        // Redirect user or perform other actions
+      })
+      .catch((error) => {
+        // Handle login error
+        console.error('Login error:', error);
+      });
+  }
+    
 }
+
+
