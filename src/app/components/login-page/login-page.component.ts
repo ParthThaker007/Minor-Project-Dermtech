7ebc6b229add4864a7af0e2703ab58bacd52e8a2
ParthThaker007 <101121538+ParthThaker007@users.noreply.github.com>
2024-05-17 14:21:20 +0530

    New Commit

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 
+import { NgForm } from '@angular/forms';
+
+import { Router } from '@angular/router';
+
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router'; 
+
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -12,32 +16,23 @@ import { Router, RouterLink } from '@angular/router';
 export class LoginPageComponent {
   email: string = '';
   password: string = '';
-  loginForm: FormGroup;
 
-  constructor(private auth: AngularFireAuth, private fb: FormBuilder, private router: Router) {
-    this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]]
-    });
-  }
+  
+  
 
-  login() {
-    if (this.loginForm.invalid) {
-      console.log('Invalid form');
+  constructor(private afAuth: AngularFireAuth, private router: Router) {}
+
+  async login(form: NgForm) {
+    if (form.invalid) {
       return;
     }
 
-    console.log('Login button clicked');
-    this.auth.signInWithEmailAndPassword(this.loginForm.value.email, this.loginForm.value.password)
-      .then((userCredential) => {
-        // Login successful
-        console.log('User logged in:', userCredential.user);
-        this.router.navigate(['/home']);
-      })
-      .catch((error) => {
-        // Handle login error
-        console.error('Login error:', error);
-      });
+    try {
+      const result = await this.afAuth.signInWithEmailAndPassword(this.email, this.password);
+      console.log(result);
+      this.router.navigate(['/home']);
+    } catch (error) {
+      console.error('Login error:', error); 
   }
 }
-
+}
